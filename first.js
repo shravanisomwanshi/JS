315627945ffdfd7b74e3c99527decb9fef8e0f59
Shravani Somwanshi <shravanisomwanshi88@gmail.com>
2026-01-17 20:38:47 +0530

    Day 1

---
+++ first.js
@@ -0,0 +1,66 @@
+// console.log(" Welcome to Apna college");
+// console.log("shraddha khapra");
+// console.log("javascript tutorials");
+
+
+// fullName = "Tony Stark";
+// age = 24;
+// price = 99.99;
+// radius = 14;
+// x = null;
+// y = undefined;
+// isfollow = false;
+
+// fullname = 25;
+
+// console.log(fullname);
+
+//---------------------------------------------------- 
+
+// const student = {
+//     fullName : "Rahul kumar",
+//     age: 20,
+//     cgpa: 8.2,
+//     ispass: true,
+// };
+
+// student["name"] = "Rahul kumar";
+
+// console.log(student["age"]);
+
+// -----------------------------------------------
+
+// practice question
+
+// const product = {
+//     title: "Ball pen",
+//     rating: 4,
+//     offer: 5,
+//     price: 270,
+// };
+
+// console.log(product);
+
+// --------------------------------------------------------
+
+// practice question
+
+// const profile = {
+//     username: "shraddhakhapra",
+//     isfollow: false,
+//     followers: 123,
+//     following: 123,
+// };
+
+// console.log(profile);
+// -------------------------------------------------------
+
+// Arithmetic operators
+let a = 5;
+let b =2;
+
+console.log("a = ", a, " & b = ", b);
+console.log("a + b = ", a + b);
+console.log("a - b = ", a - b);
+console.log("a * b = ", a * b);
+console.log("a / b = ", a / b);
